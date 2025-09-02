b1ee41f2eecd8ca95cf7cf27973e945a8eabb93f
madil002 <muabdul.adil@gmail.com>
2025-09-02 16:46:56 +0100

    reduced repetition

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,15 @@ module "IdP" {
   aws_account_id = var.aws_account_id
 }
 
+locals {
+  subnets = {
+    public_a  = { cidr = "10.0.1.0/24", az = "eu-west-2a", type = "public" }
+    private_a = { cidr = "10.0.2.0/24", az = "eu-west-2a", type = "private" }
+    public_b  = { cidr = "10.0.3.0/24", az = "eu-west-2b", type = "public" }
+    private_b = { cidr = "10.0.4.0/24", az = "eu-west-2b", type = "private" }
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
@@ -14,33 +23,15 @@ resource "aws_internet_gateway" "igw" {
   tags   = { Name = "Main-igw" }
 }
 
-resource "aws_subnet" "public_a" {
+resource "aws_subnet" "all" {
+  for_each = local.subnets
+
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "eu-west-2a"
-  tags              = { Name = "public-subnet-a" }
+  cidr_block        = each.value.cidr
+  availability_zone = each.value.az
+  tags              = { Name = each.key }
 }
 
-resource "aws_subnet" "private_a" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "eu-west-2a"
-  tags              = { Name = "private-subnet-a" }
-}
-
-resource "aws_subnet" "public_b" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "eu-west-2b"
-  tags              = { Name = "public-subnet-b" }
-}
-
-resource "aws_subnet" "private_b" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "eu-west-2b"
-  tags              = { Name = "private-subnet-b" }
-}
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
@@ -56,22 +47,9 @@ resource "aws_route_table" "private" {
   tags   = { Name = "Main-private-rt" }
 }
 
-resource "aws_route_table_association" "public_a" {
-  subnet_id      = aws_subnet.public_a.id
-  route_table_id = aws_route_table.public.id
-}
+resource "aws_route_table_association" "all" {
+  for_each = local.subnets
 
-resource "aws_route_table_association" "public_b" {
-  subnet_id      = aws_subnet.public_b.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "private_a" {
-  subnet_id      = aws_subnet.private_a.id
-  route_table_id = aws_route_table.private.id
-}
-
-resource "aws_route_table_association" "private_b" {
-  subnet_id      = aws_subnet.private_b.id
-  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.all[each.key].id
+  route_table_id = each.value.type == "public" ? aws_route_table.public.id : aws_route_table.private.id
 }
