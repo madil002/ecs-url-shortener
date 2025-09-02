6346ef05f2e966430a74aae33190aede381da8cb
madil002 <muabdul.adil@gmail.com>
2025-09-02 02:10:42 +0100

    modularised IdP

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,4 @@
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["2b18947a6a9fc7764fd8b5fb18a863b0c6dac24f"]
-}
-
-resource "aws_iam_role" "GitHubActions-ECR-Push" {
-  name = "GitHubActions-ECR-Push"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Federated = "arn:aws:iam::${var.aws_account_id}:oidc-provider/token.actions.githubusercontent.com"
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub" = "repo:madil002/ecs-url-shortener:ref:refs/heads/main"
-          }
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecr" {
-  role       = aws_iam_role.GitHubActions-ECR-Push.id
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+module "IdP" {
+  source         = "./modules/IdP"
+  aws_account_id = var.aws_account_id
 }
