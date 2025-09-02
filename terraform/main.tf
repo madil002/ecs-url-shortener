ad5bafb4f6a1803bd7e48c229c05ecd2c0c27073
madil002 <muabdul.adil@gmail.com>
2025-09-02 01:35:04 +0100

    modify IdP role policy to aws managed

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,8 @@ resource "aws_iam_openid_connect_provider" "github" {
   thumbprint_list = ["2b18947a6a9fc7764fd8b5fb18a863b0c6dac24f"]
 }
 
-resource "aws_iam_role" "GitHubActions-ECR-Push-2" {
-  name = "GitHubActions-ECR-Push-2"
+resource "aws_iam_role" "GitHubActions-ECR-Push" {
+  name = "GitHubActions-ECR-Push"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -26,33 +26,7 @@ resource "aws_iam_role" "GitHubActions-ECR-Push-2" {
   })
 }
 
-resource "aws_iam_role_policy" "AmazonEC2ContainerRegistryPowerUser" {
-  name = "AmazonEC2ContainerRegistryPowerUser"
-  role = aws_iam_role.GitHubActions-ECR-Push-2.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "ecr:GetAuthorizationToken",
-        "ecr:BatchCheckLayerAvailability",
-        "ecr:GetDownloadUrlForLayer",
-        "ecr:GetRepositoryPolicy",
-        "ecr:DescribeRepositories",
-        "ecr:ListImages",
-        "ecr:DescribeImages",
-        "ecr:BatchGetImage",
-        "ecr:GetLifecyclePolicy",
-        "ecr:GetLifecyclePolicyPreview",
-        "ecr:ListTagsForResource",
-        "ecr:DescribeImageScanFindings",
-        "ecr:InitiateLayerUpload",
-        "ecr:UploadLayerPart",
-        "ecr:CompleteLayerUpload",
-        "ecr:PutImage"
-      ]
-      Resource = "*"
-    }]
-  })
+resource "aws_iam_role_policy_attachment" "ecr" {
+  role       = aws_iam_role.GitHubActions-ECR-Push.id
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
 }
