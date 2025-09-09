80dc5f243ac3eedc2deda725ac5cd503826c2bf7
madil002 <muabdul.adil@gmail.com>
2025-09-09 23:51:35 +0100

    added iam role for terraform workflows

--- terraform/modules/IdP/main.tf
+++ terraform/modules/IdP/main.tf
@@ -30,3 +30,58 @@ resource "aws_iam_role_policy_attachment" "ecr" {
   role       = aws_iam_role.GitHubActions-ECR-Push.id
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
 }
+
+resource "aws_iam_role" "GitHubActions-Terraform" {
+  name = "GitHubActions-Terraform"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${var.aws_account_id}:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
+            "token.actions.githubusercontent.com:sub" = "repo:madil002/ecs-url-shortener:ref:refs/heads/main"
+          }
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "terraform" {
+  name = "GithubActions-Terraform"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "acm:*",
+          "route53:*",
+          "ec2:*",
+          "elasticloadbalancing:*",
+          "ecs:*",
+          "codedeploy:*",
+          "dynamodb:*",
+          "wafv2:*",
+          "logs:*",
+          "cloudwatch:*",
+          "iam:*",
+          "s3:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "tf" {
+  role       = aws_iam_role.GitHubActions-Terraform.id
+  policy_arn = aws_iam_policy.terraform.arn
+}
